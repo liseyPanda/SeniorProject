5cbd4f1a0ab1ce10e92f5078363c6c44df8e21d6
Elise <epfalck.ufl.edu>
2017-12-06 20:52:41 -0500

    added new testing and css

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 //==============jQuery functions for simulation===============//
 $(document).ready(function() {
-  //Initialize all variables needed for calculations
   var rocket = $('#rocket'),
       gravity = -9.80665,
-      abs_g = Math.abs(gravity),
-      angle = 0,
-      v = 0, //Initial Velocity
-      t = 0,
-      x = 0, // X Position
-      y = 0; // Y Position
+      abs_g = Math.abs(gravity), //absolute value to convert gravity
+      projectile_ang = 0, //initialize angle of projection
+      v = 0, //initialize velocity
+      t = 0, //t for time
+      x = 0, // horizontal axis position
+      y = 0; // verticle axis position
 
-  // Function to Move the Ball with CSS positioning
+  // Move rocket
   function moveRocket(x, y) {
     rocket.css({
       left: x,
@@ -18,31 +17,31 @@ $(document).ready(function() {
     });
   }
 
-  // Function to Convert angle to Radians for JS
-  function toRadians (angle) {
-    return angle * (Math.PI / 180);
+  // Radians for JavaScript
+  function toRadians (projectile_ang) {
+    return projectile_ang * (Math.PI / 180);
   }
 
   // Function to Calculate Y
-  function findY(gravity, angle, v, t) {
+  function calc_Y(gravity, projectile_ang, v, t) {
     var t_squared = Math.pow(t,2),
-        sin_angle = Math.sin(toRadians(angle));
-    return Math.round( (sin_angle*v*t) + ((0.5)*gravity*t_squared) );
+        sin_projectile_ang = Math.sin(toRadians(projectile_ang));
+    return Math.round( (sin_projectile_ang*v*t) + ((0.5)*gravity*t_squared) );
   }
 
   // Function to Calculate X
-  function findX(angle, v, t) {
-    var cos_angle = Math.cos(toRadians(angle));
-    return Math.round(cos_angle*v*t);
+  function calc_X(projectile_ang, v, t) {
+    var cos_projectile_ang = Math.cos(toRadians(projectile_ang));
+    return Math.round(cos_projectile_ang*v*t);
   }
 
   // Function to Calculate Max Time
-  function maxTime(angle, v, gravity) {
-    var sin_angle = Math.sin(toRadians(angle));
-    return ((sin_angle*v)/abs_g)*2;
+  function maxTime(projectile_ang, v, gravity) {
+    var sin_projectile_ang = Math.sin(toRadians(projectile_ang));
+    return ((sin_projectile_ang*v)/abs_g)*2;
   }
 
-  function simulate(v, angle) {
+  function simulate(v, projectile_ang) {
     // Reset Everything
     $('.line').remove();
     rocket.css({
@@ -50,14 +49,14 @@ $(document).ready(function() {
       right: 0
     });
 
-    var max_time = maxTime(angle, v, gravity),
+    var max_time = maxTime(projectile_ang, v, gravity),
         max_time_100 = (max_time/100);
 
     function calculatePosition(i) {
       t = i*(max_time_100);
       console.log("After " + t + " seconds:");
-      x = findX(angle, v, t);
-      y = findY(gravity, angle, v, t);
+      x = calc_X(projectile_ang, v, t);
+      y = calc_Y(gravity, projectile_ang, v, t);
 
       console.log(x);
       console.log(y);
@@ -71,17 +70,17 @@ $(document).ready(function() {
       i++;
     }
 
-    function setIntervalX(callback, delay, repetitions) {
+    function increment_X(callback, delay, repetitions) {
       // Disable Button
       $('#simulate').prop('disabled', true);
 
       var x = 0;
-      var intervalID = window.setInterval(function () {
+      var increment = window.setInterval(function () {
 
         calculatePosition(x);
 
         if (++x === repetitions) {
-          window.clearInterval(intervalID);
+          window.clearInterval(increment);
 
           // Re-enable Button
           $('#simulate').prop('disabled', false);
@@ -89,7 +88,7 @@ $(document).ready(function() {
       }, delay);
     }
 
-    setIntervalX(function () {}, (max_time*10), 101);
+    increment_X(function () {}, (max_time*10), 101);
   }
 //===========Velocity Slide Bar=============//
    var slider = document.getElementById("myVelocity");
@@ -100,7 +99,7 @@ $(document).ready(function() {
    output.innerHTML = this.value;
    console.log(output.innerHTML);
  }
-//===========Angle of Trajectory Slide Bar=============//
+//===========projectile_ang of Trajectory Slide Bar=============//
   var slider2 = document.getElementById("myAngle");
   var output2 = document.getElementById("ang_output");
   output2.innerHTML = slider2.value;
@@ -116,8 +115,8 @@ $(document).ready(function() {
 
     var velocity_input = output.innerHTML;
 
-    var angle_input = output2.innerHTML;
+    var projectile_ang_input = output2.innerHTML;
 
-    simulate(velocity_input, angle_input);
+    simulate(velocity_input, projectile_ang_input);
   });
 });
